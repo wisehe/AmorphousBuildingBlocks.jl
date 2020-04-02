cbab3c8446fabac8ab6179b5730673e7b1927dd2
Henry Wise <wisehe@oregonstate.edu>
2020-04-02 12:18:47 -0700

    Files generated by PkgTemplates

---
+++ src/AmorphousBuildingBlocks.jl
@@ -0,0 +1,5 @@
+module AmorphousBuildingBlocks
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AmorphousBuildingBlocks
+using Test
+
+@testset "AmorphousBuildingBlocks.jl" begin
+    # Write your own tests here.
+end
